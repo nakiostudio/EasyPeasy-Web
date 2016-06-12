57828421a5f630ccd1e670629f918ab23b067cd0
Carlos Vidal <nakioparkour@gmail.com>
2016-06-12 17:54:09 +0100

    rc-1

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,34 @@
-var pageHeight;
-var contentHeight;
-var pages;
-
-function puts(string) {
-  console.log(string);
-}
-
-function offsetForPage(page) {
-  var initial = page * pageHeight;
-  var final = initial + pageHeight;
-  var offset = $(document).scrollTop();
-
-  var visibleHeight = Math.abs(pageHeight - initial - offset);
-  visibleHeight = Math.min(pageHeight, Math.max(0, visibleHeight));
-
-  return (visibleHeight / pageHeight) * 100;
-}
+// var pageHeight;
+// var contentHeight;
+// var pages;
+//
+// function puts(string) {
+//   console.log(string);
+// }
+//
+// function offsetForPage(page) {
+//   var initial = page * pageHeight;
+//   var final = initial + pageHeight;
+//   var offset = $(document).scrollTop();
+//
+//   var visibleHeight = Math.abs(pageHeight - initial - offset);
+//   visibleHeight = Math.min(pageHeight, Math.max(0, visibleHeight));
+//
+//   return (visibleHeight / pageHeight) * 100;
+// }
 
 $(document).ready(function(){
   pageHeight = $(window).height();
   contentHeight = $(document).height();
   pages = contentHeight / pageHeight;
+
+  $(".github-link").on( "click", function() {
+    window.open("https://github.com/nakiostudio/EasyPeasy", "_self");
+  });
 });
 
-$(window).scroll(function() {
-  puts("Page 0: " + offsetForPage(0));
-  puts("Page 1: " + offsetForPage(1));
-  puts("Page 2: " + offsetForPage(2));
-});
+// $(window).scroll(function() {
+//   puts("Page 0: " + offsetForPage(0));
+//   puts("Page 1: " + offsetForPage(1));
+//   puts("Page 2: " + offsetForPage(2));
+// });
